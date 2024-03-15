411904445c5a7ad5fd170a194fc862f9cea5d361
krysthopher@gmail.com <krysthopher@gmail.com>
2024-03-15 17:31:42 -0600

    start of splitting all code challenges

--- Challenges.js
+++ Challenges.js
@@ -1,25 +1,7 @@
 // This is compilation of the challenges I have done so far on CodeWars
 
 
-//Title:  Complementary DNA
-//link: https://www.codewars.com/kata/554e4a2f232cdd87d9000038
 
-function dnaStrand(dna) {
-    let array = dna.split("");
-    let arrayTwo = [];
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === "A") {
-        arrayTwo.push("T");
-        } else if (array[i] === "T") {
-        arrayTwo.push("A");
-        } else if (array[i] === "G") {
-        arrayTwo.push("C");
-        } else if (array[i] === "C") {
-        arrayTwo.push("G");
-        }
-    }
-    return arrayTwo.join("");
-}
 
 
 //Grasshopper - Messi goals function
---
+++ dnaStrand.js
@@ -0,0 +1,19 @@
+//Title:  Complementary DNA
+//link: https://www.codewars.com/kata/554e4a2f232cdd87d9000038
+
+function dnaStrand(dna) {
+    let array = dna.split("");
+    let arrayTwo = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === "A") {
+        arrayTwo.push("T");
+        } else if (array[i] === "T") {
+        arrayTwo.push("A");
+        } else if (array[i] === "G") {
+        arrayTwo.push("C");
+        } else if (array[i] === "C") {
+        arrayTwo.push("G");
+        }
+    }
+    return arrayTwo.join("");
+}
