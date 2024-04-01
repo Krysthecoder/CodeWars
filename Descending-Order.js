fb8aad89f8093137c26892830a89141eb139cb8f
krysthopher@gmail.com <krysthopher@gmail.com>
2024-04-01 15:27:41 -0600

    Daily practice 4-1-2024

---
+++ Descending-Order.js
@@ -0,0 +1,4 @@
+//Title: Descending Order
+//Link: https://www.codewars.com/kata/5467e4d82edf8bbf40000155/solutions/javascript
+
+const descendingOrder = n => n.toString().split('').sort().reverse().join('') * 1
