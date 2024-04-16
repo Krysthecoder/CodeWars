5884017e8c695ff01ad3299a1a0c1dc0625f80ab
krysthopher@gmail.com <krysthopher@gmail.com>
2024-04-15 19:20:10 -0600

    added training 19, learned that its better to use slice than replace in some cases

---
+++ Training-JS/Training-19.js
@@ -0,0 +1,15 @@
+//Title: Training JS #19: Methods of String object--toUpperCase() toLowerCase() and replace()
+//https://www.codewars.com/kata/5728203b7fc662a4c4000ef3/solutions/javascript?filter=me&sort=best_practice&invalids=false
+
+function alienLanguage(str) {
+  str = str.toUpperCase().split(" ");
+  for (let i = 0; i < str.length; i++) {
+    str[i] = str[i].slice(0, -1) + str[i][str[i].length - 1].toLowerCase();
+  }
+  return str.join(" ");
+}
+
+alienLanguage("My name is John"); // , "My NAMe Is JOHn");
+alienLanguage("this is an example"); // , "THIs Is An EXAMPLe");
+alienLanguage("Hello World"); // , "HELLo WORLd");
+alienLanguage("HELLO WORLD"); // , "HELLo WORLd");
