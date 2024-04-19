2a95eff9ac7d0fc98b76b6015e72e2379d66fb98
krysthopher@gmail.com <krysthopher@gmail.com>
2024-04-18 20:55:08 -0600

    added currying function challenges

---
+++ CurryingFunction.js
@@ -0,0 +1,18 @@
+//Title: Currying functions: multiply all elements in an array
+//Link: https://www.codewars.com/kata/586909e4c66d18dd1800009b/train/javascript
+
+//I did not know I have to use currying functions so I tried to make it like this:
+
+function multiplyAll(...args) {
+  const [arr, multiplier] = args;
+  return arr.map((digits) => digits * multiplier);
+}
+
+//but using currying functions looks nice ngl,
+
+const multiplyAll = (arr) => (integer) => {
+  let newArr = arr.map((element) => element * integer);
+  return newArr;
+};
+//final submition
+const multiplyAll = (arr) => (integer) =>  arr.map((element) => element * integer);
